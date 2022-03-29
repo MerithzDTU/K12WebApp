40d3c2cc0accbd04aa0018d79a3fbadccde89838
Christian Merithz <s195480@student.dtu.dk>
2022-03-29 16:19:27 +0200

    Start of authorization

--- K12WebApp/Client/Program.cs
+++ K12WebApp/Client/Program.cs
@@ -1,6 +1,7 @@
 global using K12WebApp.Client.Services.UserService;
 global using K12WebApp.Client.Services.GroceryService;
 global using K12WebApp.Shared;
+global using Microsoft.AspNetCore.Components.Authorization;
 using K12WebApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,5 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IGroceryService, GroceryService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
