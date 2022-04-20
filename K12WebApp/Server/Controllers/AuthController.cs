db3e90311afeb92dcb5d952e5cf760ea23c914bc
Christian Merithz <s195480@student.dtu.dk>
2022-04-20 19:15:07 +0200

    Registration virker igennem front-end

--- K12WebApp/Server/Controllers/AuthController.cs
+++ K12WebApp/Server/Controllers/AuthController.cs
@@ -116,9 +116,11 @@ namespace K12WebApp.Server.Controllers
 
         private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using var hmac = new HMACSHA512();
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
 
         private static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
@@ -126,7 +128,7 @@ namespace K12WebApp.Server.Controllers
             using (var hmac = new HMACSHA512(passwordSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                Console.WriteLine("COMPUTED HASH -----> " + computedHash + "PASSWORDHASH -----> " + passwordHash);
+                Console.WriteLine("COMPUTED HASH -----> " + computedHash.ToString() + "PASSWORDHASH -----> " + passwordHash.ToString());
                 return computedHash.SequenceEqual(passwordHash);
             }
         }
