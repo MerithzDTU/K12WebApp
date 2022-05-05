6fc7b62b7edcb27bdfa0019937b813b27132f3a6
Christian Merithz Uhrenfeldt Nielsen <s195480@student.dtu.dk>
2022-05-05 10:08:37 +0200

    Adding chores (needs fixing)

---
+++ K12WebApp/Shared/ChoreMonth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace K12WebApp.Shared
+{
+	public class ChoreMonth
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public ICollection<User> AssignedUser { get; set; } 
+		public List<int> AssignedUserId { get; set; }
+
+	}
+}
+
